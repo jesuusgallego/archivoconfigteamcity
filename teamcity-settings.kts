4b2aae276e2d2e3c4315fd2210d7475d0a2b7ad5
jesuusgallego <jesus.gallegoiba@alum.uca.es>
2025-01-02 18:57:49 +0100

    Update teamcity-settings.kts

--- teamcity-settings.kts
+++ teamcity-settings.kts
@@ -4,15 +4,12 @@ version = "2023.2"
 
 project {
     name = "LocalPythonProject"
-    id("LocalPythonProject")
 
-    buildType(LocalPythonProject_RunHelloWorldScript)
-    vcsRoot(LocalPythonProject_HttpsGithubComJesuusgallegoArchivoconfigteamcityGitRefsHeadsMain)
+    buildType(RunHelloWorldScript)
 }
 
-object LocalPythonProject_RunHelloWorldScript : BuildType({
+object RunHelloWorldScript : BuildType({
     name = "Run Hello World Script"
-    id("LocalPythonProject_RunHelloWorldScript")
 
     steps {
         script {
@@ -22,20 +19,5 @@ object LocalPythonProject_RunHelloWorldScript : BuildType({
             """.trimIndent()
         }
     }
-
-    vcs {
-        root(LocalPythonProject_HttpsGithubComJesuusgallegoArchivoconfigteamcityGitRefsHeadsMain)
-    }
 })
 
-object LocalPythonProject_HttpsGithubComJesuusgallegoArchivoconfigteamcityGitRefsHeadsMain : GitVcsRoot({
-    name = "GitHub Repo"
-    id("LocalPythonProject_HttpsGithubComJesuusgallegoArchivoconfigteamcityGitRefsHeadsMain")
-
-    url = "https://github.com/jesuusgallego/archivoconfigteamcity.git"
-    branch = "refs/heads/main"
-    authMethod = password {
-        userName = "jesuusgallego"
-        password = "11032002116j"
-    }
-})
