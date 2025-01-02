cda3f70032bc3be33da7a4b266a290ae555116d1
jesuusgallego <jesus.gallegoiba@alum.uca.es>
2025-01-02 17:51:58 +0100

    Update teamcity-settings.kts
    Archivo de configuración del pipeline en TeamCity

---
+++ teamcity-settings.kts
@@ -0,0 +1,32 @@
+import jetbrains.buildServer.configs.kotlin.v2023_2.*
+
+version = "2023.2"
+
+project {
+    name = "LocalPythonProject"
+
+    buildType(RunHelloWorldScript)
+}
+
+object RunHelloWorldScript : BuildType({
+    name = "Run Hello World Script"
+
+    steps {
+        script {
+            name = "Run Python Script"
+            scriptContent = """
+                python3 /opt/teamcity/hello_world.py
+            """.trimIndent()
+        }
+    }
+
+    vcs {
+        root(DslContext.settingsRoot)
+    }
+
+    triggers {
+        vcs {
+            branchFilter = "+:*"
+        }
+    }
+})
